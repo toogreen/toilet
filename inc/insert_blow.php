e9de618c526424baaeb22c457426b0b21a8d72c6
David Gagnon <toogreen@gmail.com>
2020-06-26 01:04:46 +0000

    initial commit

---
+++ inc/insert_blow.php
@@ -0,0 +1,20 @@
+<?php
+include("connect_db.php");
+
+$company=($_POST["company"]);
+$comment=($_POST["comment"]);
+$position=($_POST["position"]);
+
+$sql = "INSERT INTO blow (company, comment, position) VALUES ('$company', '$comment', '$position')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+header("Location: ../index.php");
+exit();
+?>
