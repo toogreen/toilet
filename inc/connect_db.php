15240267daa5cddc1205283a5a38e8d3d20136bb
David Gagnon <toogreen@gmail.com>
2020-06-26 02:42:36 +0000

    updated generic connect.db

---
+++ inc/connect_db.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "user";
+$password = "pass";
+$dbname = "dbname";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$table = "wall";
+$id = "id";
+$field1 = "title";
+$field2 = "comment";
+$field3 = "signature";
